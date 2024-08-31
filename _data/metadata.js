49771eb282446e41db5939cfa247bf02418b31ed
Alexander Dubovoy <ajdubovoy@gmail.com>
2024-08-31 16:04:18 +0200

    Start project

---
+++ _data/metadata.js
@@ -0,0 +1,14 @@
+let data = {
+  title: "Brazilian Vibes",
+  version: process.env.npm_package_version,
+  repo: "https://github.com/ajdubovoy/brazilian-vibes",
+  url: "https://brazilian-vibes.netlify.app",
+  language: "en",
+  description: "Brazilian live music, presented regularly in Berlin.",
+  author: {
+    name: "Alexander Dubovoy",
+    url: "https://www.alexanderdubovoy.com",
+  },
+};
+
+export default data;
